5b93b937bcc95717a38fe271fcfd67b9119afe1a
Glenda Durán <130619011+glnd-drn@users.noreply.github.com>
2023-07-10 19:59:11 +0000

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,34 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  console.log(variables);
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables["name"] == null ? " " : variables.name} ${
+    variables["lastname"] == null ? " " : variables.lastname
+  } </h1>
+          <h2>${variables["role"] == null ? " " : variables.role}</h2>
+          <h3>${variables["country"] == null ? " " : variables.country} ${
+    variables["city"] == null ? " " : variables.city
+  } </h3>
+  <ul class="${
+    variables["socialMediaPosition"] == null
+      ? ""
+      : variables["socialMediaPosition"]
+  }">
+          <li><a href="https://twitter.com/ ${
+            variables["twitter"] == null ? "" : variables["twitter"]
+          } "><i class="fab fa-twitter"></i></a></li>
+          <li><a href="https://github.com/ ${
+            variables["github"] == null ? "" : variables["github"]
+          } "><i class="fab fa-github"></i></a></li>
+          <li><a href="https://linkedin.com/ ${
+            variables["linkedin"] == null ? "" : variables["linkedin"]
+          } "><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/ ${
+              variables["instagram"] == null ? "" : variables["instagram"]
+            } "><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +70,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: "https://images5.alphacoders.com/349/349087.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://avatars.githubusercontent.com/u/130619011?s=400&u=526755535e90596337a55fa135df7bbbdc128fd6&v=4",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
